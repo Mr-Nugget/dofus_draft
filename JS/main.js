ddee453aa63f40e59591d0fccd9984197d94d7dd
Titouan Raimbault <t_raimbault@yahoo.fr>
2018-11-20 11:44:16 +0100

    Ban equipe B a gauche, modification des boutons reset et back

--- JS/main.js
+++ JS/main.js
@@ -5,13 +5,23 @@ var pick = [false, false, true, true, false, false, false, false, true, true, fa
 var team = [1,2,1,2,2,1,2,1,2,1,1,2,1,2];
 var undo = new Array();
 
+window.onload = hello;
+
+function hello() {
+    $("section").fadeIn(300);
+}
+
+
 function myclick (perso){
     undo.push(perso);
     var id = perso.attributes["id"].value;
     $("#back").prop('disabled', false);
     if(pick[cpt]){
-        $("#img_pick"+cpt_pick).attr('src', 'res/'+id+'_carte.jpg');
-        $("#"+id+" .pick-img").fadeIn(300);
+      var temp = cpt_pick;
+        $("#img_pick"+temp).fadeOut(0, function(){
+            $("#img_pick"+temp).attr('src', 'res/'+id+'_carte.jpg');
+          }).fadeIn(300);
+        $("#"+id+" .pick-img").fadeIn(600);
         cpt_pick++;
     }else{
         $("#"+id+" .ban-img").fadeIn(300);
@@ -26,11 +36,23 @@ function myclick (perso){
           $("#pillier div, #sous-pillier div, #erodeur div").css("cursor", "default");
           $("#pillier div, #sous-pillier div, #erodeur div").attr("onclick", "");
           $("#pillier div, #sous-pillier div, #erodeur div").css("opacity", "0.5");
-          $("#back").prop('disabled', true);
           $("#back img").css('cursor', 'default');
           $("#back img").css('opacity', '0.5');
+          $("#classes h3").css('opacity', '0.5');
+          $("#back").css("cursor", "default");
+          $("#back").css("opacity", "0.5");
+          $("#back").prop("disabled", true);
         }
     }
+    if(cpt == 1){
+      $("#reset").css("cursor", "pointer");
+      $("#back").css("cursor", "pointer");
+      $("#reset").css("opacity", "1");
+      $("#back").css("opacity", "1");
+      $("#back").prop('disabled', false);
+      $("#reset").prop('disabled', false);
+    }
+
     perso.onclick = null;
 }
 
@@ -40,6 +62,11 @@ function back(){
     $("#fleches").children().eq(cpt).css("opacity", 1);
     if(cpt == 0){
         $("#back").prop('disabled', true);
+        $("#reset").prop('disabled', true);
+        $("#reset").css("cursor", "default");
+        $("#back").css("cursor", "default");
+        $("#reset").css("opacity", "0.5");
+        $("#back").css("opacity", "0.5");
     }
     var indice = undo.length -1;
     var id = undo[indice].attributes["id"].value;
@@ -69,11 +96,18 @@ function reset(){
 
     $("#back img").css('cursor', 'pointer');
     $("#back img").css('opacity', '1');
+    $("#classes h3").css('opacity', '1');
 
     $(".pick-img").fadeOut(300);
     $(".ban-img").fadeOut(300);
 
     $("#fleches img").css("opacity", "1");
+
+    $("#reset").css("cursor", "default");
+    $("#back").css("cursor", "default");
+    $("#reset").css("opacity", "0.5");
+    $("#back").css("opacity", "0.5");
+
 
     for (var i = 0; i < 8; i++) {
       $("#img_ban"+i).attr('src', 'res/basic_ban.png');
