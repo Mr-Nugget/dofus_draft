ae538a1a1650e40111f2b9318d3ee701e18270f3
Titouan RAIMBAULT <t_raimbault@yahoo.fr>
2020-05-28 16:53:09 +0200

    Update dofus draft by adding bootstrap and redesign main panel, add fonctionality to re click on a character to cancel the add

--- JS/main.js
+++ JS/main.js
@@ -4,46 +4,60 @@ var cpt_ban = 0;
 var pick = [false, false, true, true, false, false, false, false, true, true, false, false, true, true];
 var team = [1,2,1,2,2,1,2,1,2,1,1,2,1,2];
 var undo = new Array();
+var redoIndex = 0;
+var undoPositions = [];
 
-window.onload = hello;
-
-function hello() {
-    $("section").fadeIn(300);
+window.onload = () => {
+  $('#main').fadeIn(300);
+  $('#spinner').fadeOut(20);
 }
 
-
-function myclick (perso){
-    undo.push(perso);
+function myclick(perso){
+    var currentPosition;
+    var currentPick;
+    var currentBan;
+    if(undoPositions.length > 0){
+      currentPosition = undoPositions[0];
+      undo[currentPosition] = perso;
+      undoPositions.shift();
+      currentPick = getPickPosition(currentPosition);
+      currentBan = getBanPosition(currentPosition);
+    }else{
+      if(cpt >= pick.length){
+        return;
+      }
+      undo.push(perso);
+      currentPosition = cpt;
+      currentBan = cpt_ban;
+      currentPick = cpt_pick;
+      $("#fleches").children().eq(cpt).css("opacity", 0.2);
+      cpt++;
+    }
     var id = perso.attributes["id"].value;
     $("#back").prop('disabled', false);
-    if(pick[cpt]){
-      var temp = cpt_pick;
-        $("#img_pick"+temp).fadeOut(0, function(){
-            $("#img_pick"+temp).attr('src', 'res/'+id+'_carte.jpg');
+    if(pick[currentPosition]){
+        $("#img_pick"+currentPick).fadeOut(0, function(){
+            $("#img_pick"+currentPick).attr('src', 'res/'+id+'_carte.jpg');
           }).fadeIn(300);
-        $("#"+id+" .pick-img").fadeIn(600);
+        $("#"+id+" .pick-img").hide().fadeIn(600);
         cpt_pick++;
     }else{
         $("#"+id+" .ban-img").fadeIn(300);
-        $("#img_ban"+cpt_ban).attr('src', 'res/'+id+'_ban.jpg');
+        $("#img_ban"+currentBan).attr('src', 'res/'+id+'_ban.jpg');
         cpt_ban++;
     }
-    if(cpt >= pick.length){
-    }else{
-        $("#fleches").children().eq(cpt).css("opacity", 0.2);
-        cpt++;
-        if(cpt >= pick.length){
-          $("#pillier div, #sous-pillier div, #erodeur div").css("cursor", "default");
-          $("#pillier div, #sous-pillier div, #erodeur div").attr("onclick", "");
-          $("#pillier div, #sous-pillier div, #erodeur div").css("opacity", "0.5");
-          $("#back img").css('cursor', 'default');
-          $("#back img").css('opacity', '0.5');
-          $("#classes h3").css('opacity', '0.5');
-          $("#back").css("cursor", "default");
-          $("#back").css("opacity", "0.5");
-          $("#back").prop("disabled", true);
-        }
-    }
+
+    /*if(cpt >= pick.length){
+      $("#pillier div, #sous-pillier div, #erodeur div").css("cursor", "default");
+      $("#pillier div, #sous-pillier div, #erodeur div").attr("onclick", "");
+      $("#pillier div, #sous-pillier div, #erodeur div").css("opacity", "0.5");
+      $("#back img").css('cursor', 'default');
+      $("#back img").css('opacity', '0.5');
+      $("#classes h3").css('opacity', '0.5');
+      $("#back").css("cursor", "default");
+      $("#back").css("opacity", "0.5");
+      $("#back").prop("disabled", true);
+    }*/
     if(cpt == 1){
       $("#reset").css("cursor", "pointer");
       $("#back").css("cursor", "pointer");
@@ -52,10 +66,85 @@ function myclick (perso){
       $("#back").prop('disabled', false);
       $("#reset").prop('disabled', false);
     }
-
-    perso.onclick = null;
+    perso.setAttribute("onclick", "reClick(this);");
 }
 
+function reClick(perso){
+  // Get the index of the character click
+  const indexPerso = undo.indexOf(perso);
+  undoPositions.push(indexPerso);
+  var id = perso.attributes["id"].value;
+  if(pick[indexPerso]){
+    const pickPosition = getPickPosition(indexPerso);
+    $("#img_pick"+pickPosition).fadeOut(0, function(){
+        $("#img_pick"+pickPosition).attr('src', 'res/cache.png');
+      }).fadeIn(300);
+      $("#"+id+" .pick-img").hide().fadeOut(600);
+      cpt_pick--;
+  }else{
+    const banPosition = getBanPosition(indexPerso);
+    $("#img_ban"+banPosition).attr('src', 'res/basic_ban.png');
+    $("#"+id+" .ban-img").hide().fadeOut(600);
+    cpt_ban--;
+  }
+  perso.setAttribute("onclick", "myclick(this);");
+};
+
+function getPickPosition(index){
+    switch (index) {
+      case 2:
+        return 0;
+        break;
+      case 3:
+        return 1;
+        break;
+      case 8:
+        return 2;
+        break;
+      case 9:
+        return 3;
+        break;
+      case 12:
+        return 4;
+        break;
+      case 13:
+        return 5;
+        break;
+      default:
+        return -1;
+    }
+}
+
+function getBanPosition(index){
+  switch (index) {
+    case 0:
+      return 0;
+      break;
+    case 1:
+      return 1;
+      break;
+    case 4:
+      return 2;
+      break;
+    case 5:
+      return 3;
+      break;
+    case 6:
+      return 4;
+      break;
+    case 7:
+      return 5;
+      break;
+    case 10:
+      return 6;
+      break;
+    case 11:
+      return 7;
+      break;
+    default:
+      return -1;
+    }
+}
 
 function back(){
     cpt--;
@@ -75,7 +164,7 @@ function back(){
     if(pick[cpt]){
         cpt_pick--;
         $("#"+id+" .pick-img").fadeOut(300);
-        $("#img_pick"+cpt_pick).attr('src', 'res/cache.jpg');
+        $("#img_pick"+cpt_pick).attr('src', 'res/cache.png');
 
     }else{
         $("#"+id+" .ban-img").fadeOut(300);
@@ -88,6 +177,8 @@ function reset(){
     cpt=0;
     cpt_ban =0;
     cpt_pick=0;
+    undoPositions = [];
+    undo = [];
 
     $("#pillier div, #sous-pillier div, #erodeur div").css("cursor", "pointer");
     $("#pillier div, #sous-pillier div, #erodeur div").attr("onclick", "myclick(this);");
@@ -113,7 +204,6 @@ function reset(){
       $("#img_ban"+i).attr('src', 'res/basic_ban.png');
     }
     for (var i = 0; i < 6; i++) {
-      $("#img_pick"+i).attr('src', 'res/cache.jpg');
+      $("#img_pick"+i).attr('src', 'res/cache.png');
     }
-
 }
